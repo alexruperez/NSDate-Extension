ce6595c2659fd86e23494609575d7e3fd6fa5679
Alejandro Rupérez Hernando <alexruperez@gmail.com>
2014-06-22 13:27:56 +0200

    Added example UI

--- NSDate-Extension Example/ARViewController.m
+++ NSDate-Extension Example/ARViewController.m
@@ -11,6 +11,12 @@
 #import "ARViewController.h"
 
 @interface ARViewController ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *unitControl;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *timeControl;
+@property (weak, nonatomic) IBOutlet UIStepper *stepper;
+@property (weak, nonatomic) IBOutlet UILabel *numberLabel;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
 
 @end
 
@@ -21,10 +27,53 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 
-    NSLog(@"%lu", [[NSDate date] get:[@(3).years and:@(2).months].later].year);
-    NSLog(@"%@", [[NSDate date] get:[@(11).days and:@(2).months].ago]);
+    [self refresh];
+    
     NSLog(@"%@", [[[NSDate date] get:[@(11).days and:@(2).months].ago] lapse:NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit|NSCalendarUnitHour fromDate:[NSDate date]]);
     NSLog(@"%@", [[NSDate date] lapse:NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit|NSCalendarUnitHour toDate:[[NSDate date] get:[@(11).days and:@(2).months].ago]]);
+}
+
+- (IBAction)refresh
+{
+    self.numberLabel.text = [NSString stringWithFormat:@"%.0f", self.stepper.value];
+    
+    NSNumber *stepperValue = @(self.stepper.value);
+    NSDateComponents *components;
+    
+    switch (self.unitControl.selectedSegmentIndex)
+    {
+        case 0:
+            components = stepperValue.years;
+            break;
+        case 1:
+            components = stepperValue.months;
+            break;
+        case 2:
+            components = stepperValue.days;
+            break;
+        case 3:
+            components = stepperValue.hours;
+            break;
+        case 4:
+            components = stepperValue.minutes;
+            break;
+        case 5:
+            components = stepperValue.seconds;
+            break;
+        default:
+            break;
+    }
+    
+    if (self.timeControl.selectedSegmentIndex)
+    {
+        components = components.later;
+    }
+    else
+    {
+        components = components.ago;
+    }
+    
+    self.resultLabel.text = [NSString stringWithFormat:@"%@", [self.datePicker.date get:components]];
 }
 
 @end
